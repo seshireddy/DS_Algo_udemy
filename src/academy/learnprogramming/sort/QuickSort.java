6df1b1396794995537a927b6f67c699b48bdc952
Dibyajyoti Behera <dibyajyoti_behera@dellteam.com>
2019-08-14 19:50:50 +0530

    quick sort

---
+++ src/academy/learnprogramming/sort/QuickSort.java
@@ -0,0 +1,37 @@
+package academy.learnprogramming.sort;
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		int [] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+		quickSort(intArray, 0, intArray.length);
+		for(int a: intArray) {
+			System.out.println(a);
+		}
+	}
+	public static void quickSort(int[] input, int start, int end) {
+		if(end-start < 2) 
+			return;
+		int pivotIndex = partition(input, start, end);
+		quickSort(input, start, pivotIndex);
+		quickSort(input, pivotIndex+1, end);
+	}
+	public static int partition(int[] input, int start, int end) {
+		int pivot = input[start];
+		int i = start;
+		int j = end;
+		while(i<j) {
+			//Note: empty loop body
+			while(i<j && input[--j]>=pivot);
+			if(i<j)
+				input[i]=input[j];
+			
+			//Note: empty loop body
+			while(i<j && input[++i]<=pivot);
+			if(i<j)
+				input[j]=input[i];
+		}
+		input[j] = pivot;
+		return j;
+	}
+}

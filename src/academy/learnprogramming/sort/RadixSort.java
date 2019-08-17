224cd82dd0307efdc786d9fecdbd4547876671b8
dibyajyoti47 <dibyajyoti.6025@gmail.com>
2019-08-17 21:40:56 +0530

    radix sort

--- src/academy/learnprogramming/sort/RadixSort.java
+++ src/academy/learnprogramming/sort/RadixSort.java
@@ -4,6 +4,7 @@ public class RadixSort {
 
 	public static void main(String[] args) {
 		int [] radixArray = { 4725, 4586, 1330, 8792, 1594, 5729 };
+		radixSort(radixArray, 10, 4);
 		for(int a: radixArray) {
 			System.out.println(a);
 		}
@@ -21,6 +22,19 @@ public class RadixSort {
 		for (int value: input) {
 			countArray[getDigit(position, value, radix)]++;
 		}
+		//adust the counting array
+		for (int j=1; j<radix; j++) {
+			countArray[j] += countArray[j-1];
+		}
+		int[] temp = new int[numItems];
+		for (int tempIndex =numItems-1; tempIndex >= 0;tempIndex--) {
+			temp[--countArray[getDigit(position, input[tempIndex], radix)]] = input[tempIndex];
+		}
+		//copying back to input array
+		for(int i=0;i<numItems;i++) {
+			input[i]=temp[i];
+		}
+		
 	}
 
 	private static int getDigit(int position, int value, int radix) {
